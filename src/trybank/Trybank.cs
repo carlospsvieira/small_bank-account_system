23353a2e9e27f8af97f4938b5f3987eb95518a0b
Carlos Vieira <carlepsvieira@gmail.com>
2023-07-07 10:50:32 -0300

    add deposit to balance

--- src/trybank/Trybank.cs
+++ src/trybank/Trybank.cs
@@ -94,7 +94,12 @@ public class Trybank
   // 5. Construa a funcionalidade de depositar dinheiro
   public void Deposit(int value)
   {
-    throw new NotImplementedException();
+    if (Logged == false)
+    {
+      throw new AccessViolationException("Usuário não está logado");
+    }
+
+    Bank[loggedUser, 3] += value;
   }
 
   // 6. Construa a funcionalidade de sacar dinheiro
